4290185f0b57d07e3512acc7dd19c526451e1d02
Janrey Ubane <jnry.atutsirc@gmail.com>
2026-09-02 07:17:16 +0800

    fix(bullet): add default bulletSpeed value
    - Set bulletSpeed = 200f in Bullet.cs
    
    Prevents null reference errors when bullet prefab is instantiated
    without explicit speed assignment in the inspector.

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    public float bulletSpeed;
+    public float bulletSpeed = 200f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
